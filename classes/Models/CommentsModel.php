01cfa1d8ada965f82201dbf042266ba256073b2e
miki5789 <mikiko.kobayashi3@gmail.com>
2023-11-01 09:27:22 +0000

    initial commit

---
+++ classes/Models/CommentsModel.php
@@ -0,0 +1,15 @@
+<?php
+require_once './classes/Models/AbstractModel.php';
+
+
+class CommentsModel extends AbstractModel
+{
+    const TABLE_NAME = 'comments';
+
+    public function __construct()
+    {
+        $this->table_name = self::TABLE_NAME;
+        parent::__construct();
+    }
+}
+
---
+++ classes/Validations/AbstractValidation.php
@@ -0,0 +1,6 @@
+<?php
+
+class AbstractValidation {
+    public function __construct() {
+    }
+}
